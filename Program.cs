54b86e62ade10b3e852d53e6bf2ec7162d9e7cb5
maissukkarieh21 <136175032+maissukkarieh21@users.noreply.github.com>
2024-07-20 17:14:21 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Gifts_Store_First_project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gifts_Store_First_project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ModelContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddSession(options => options.IOTimeout = TimeSpan.FromMinutes(60));
+            var app = builder.Build();
+            app.UseSession();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
